56cc2301268b3ea22d55ebac346e7b06778a1bf8
Jas-F <jasminer.franklin@gmail.com>
2021-03-07 10:34:54 -0500

    create jumbo compoent

---
+++ memory-game/src/components/Jumbotron/Jumbo.js
@@ -0,0 +1,17 @@
+// import react and bootstrap
+import React from "react";
+import {Jumbotron} from "react-bootstrap";
+// add jumbotron function
+
+function Jumbo() {
+<Jumbotron fluid>
+    <container>
+        <h1>Clicky Game</h1>
+        <h3>Click on an image to score points, click on the same image twice and you loose</h3>
+    </container>
+</Jumbotron>
+
+}
+// create react component
+
+export default Jumbo;
--- memory-game/src/components/Jumbotron/Jumbotron.js
+++
@@ -1,7 +0,0 @@
-// import react and bootstrap
-import React from "react";
-import {Jumbotron} from "react-bootstrap";
-// add jumbotron function
-
-
-// create react component
