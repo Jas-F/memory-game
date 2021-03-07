ee184e319f8233c38417b546dc14d754cc566a4f
Jas-F <jasminer.franklin@gmail.com>
2021-03-07 10:21:30 -0500

    create function nav

--- memory-game/src/components/Nav.js
+++ memory-game/src/components/Nav.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-// add nav bar bootstrap
-<Navbar bg="light">
-    <Navbar.Brand>Clicky Game</Navbar.Brand>
-</Navbar>
-<Navbar bg="light">
-    <Navbar.Brand>You Guessed -message based off user input</Navbar.Brand>
-</Navbar>
-<Navbar bg="light">
-    <Navbar.Brand>Score: -incrimenting number | Top Score: -store highest incremented number</Navbar.Brand>
-</Navbar>
+function Nav() {
+
+    return (
+    // add nav bar bootstrap
+    <div>
+    <Navbar bg="light">
+        <Navbar.Brand>Clicky Game</Navbar.Brand>
+    </Navbar>
+    <Navbar bg="light">
+        <Navbar.Brand>You Guessed -message based off user input</Navbar.Brand>
+    </Navbar>
+    <Navbar bg="light">
+        <Navbar.Brand>Score: -incrimenting number | Top Score: -store highest incremented number</Navbar.Brand>
+    </Navbar>
+    </div>
+    )
+}
+
+export default Nav;
