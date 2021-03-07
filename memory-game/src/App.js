b4318beb9cf40bf5800756ac0d2c6caca43e1308
Jas-F <jasminer.franklin@gmail.com>
2021-03-07 00:16:43 -0500

    add navbar

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -3,7 +3,7 @@
 // create components for each element on the page (nav, jumbotron, table, footer)
 // create table/grid 3 col 4 row
 // insert images into table/grid
-// import images as seeds
+// import images as seeds image
 // add event listeners to images
 // make conditional statements such as if pic id === pic id then loose game score = 0
 // if pic id 1 !== pic id then shuffle images and increment score by 1
---
+++ memory-game/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// add nav bar bootstrap
+<Navbar bg="light">
+    <Navbar.Brand>Clicky Game</Navbar.Brand>
+</Navbar>
+<Navbar bg="light">
+    <Navbar.Brand>You Guessed -message based off user input</Navbar.Brand>
+</Navbar>
+<Navbar bg="light">
+    <Navbar.Brand>Score: -incrimenting number | Top Score: -store highest incremented number</Navbar.Brand>
+</Navbar>
