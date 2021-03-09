2b99a00ee5f0d5ed7f5dae101d2585757f7a4b97
Jas-F <jasminer.franklin@gmail.com>
2021-03-08 22:11:46 -0500

    display score and end game

--- memory-game/src/components/Game/Game.js
+++ memory-game/src/components/Game/Game.js
@@ -6,59 +6,65 @@ import { Row, Col } from "react-bootstrap";
 const IMG = ["cuban.jpg", "kieser.jpg", "kiyosaki.jpg", "musk.jpg", "bett-david.jpg", "buffet.jpg", "cj.jpg", "ma.jpg", "pauly.jpg"]
 // create grid class with react component
 class Game extends React.Component {
-// build constructor with original state of image array and score
+    // build constructor with original state of image array and score
 
-constructor(props) {
-    super(props)
-    this.state={
-        clickedImages: [],
-        score: 0,
-        gameOver: false
+    constructor(props) {
+        super(props)
+        this.state = {
+            clickedImages: [],
+            score: 0,
+            gameOver: false
+        }
     }
-}
     // render images
     render() {
+        console.log(this.state)
         return (
             // create grid layout 
             // map over images
             <container>
                 {/* create score */}
-            <h1>Score:{this.state.score}</h1>
+                <h1>Score:{this.state.clickedImages.length}</h1>
+                {/* display game over when true */}
+                {this.state.gameOver && <h1>Game Over</h1>}
                 <Row>
-                    {IMG.map(item => {
-                        return (
-                            // return images
-                            <Col  >
-                                {/*  use back tick notation*/}
-                                {/* add onclick function for images being mapped */}
-                                <img src={`/Images/${item}`} onClick={() => {
-                                    // push item array 
-                                    // copy state and create new var
-                                    // add images to it 
-                                    // update state
-                                    // add if statement to only allow first time clicked images to array
-                                    if(this.state.clickedImages.includes(item)) {
+                    {
+                        !this.state.gameOver &&
+                        IMG.map(item => {
+                            return (
+                                // return images
+                                <Col  >
+                                    {/*  use back tick notation*/}
+                                    {/* add onclick function for images being mapped */}
+                                    <img src={`/Images/${item}`} onClick={() => {
+                                        // push item array 
+                                        // copy state and create new var
+                                        // add images to it 
+                                        // update state
+                                        // add if statement to only allow first time clicked images to array
+                                        if (this.state.clickedImages.includes(item)) {
 
-                                    // set new state of score to 0 display game over
-                                        let newState = {...this.state}
-                                     newState.gameOver = true
-                                     newState.Score = 0
-                                        this.setState(newState)
-                                    
-                                    }else{
-                                        // push to array
-                                        let newState = {...this.state}
-                                        newState.clickedImages.push(item)
-                                        this.setState(newState)
-                                    }
+                                            // set new state of score to 0 display game over
+                                            let newState = { ...this.state }
+                                            newState.gameOver = true
+                                            newState.score = 0
+                                            this.setState(newState)
 
-                              
-                                    console.log(this.state)
-                                }}></img>
-                            </Col>
+                                        } else {
+                                            // push to array
+                                            let newState = { ...this.state }
+                                            // newState.Score = {this.sts+ 1
+                                            newState.clickedImages.push(item)
+                                            this.setState(newState)
+                                        }
 
-                        )
-                    })}
+
+
+                                    }}></img>
+                                </Col>
+
+                            )
+                        })}
 
                 </Row>
             </container>
